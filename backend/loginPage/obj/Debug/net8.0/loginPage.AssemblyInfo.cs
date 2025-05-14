8f00fcf6456f6b600bf94e509f7071c1bda1e5a5
mohamed12344556 <ma5123@fayoum.edu.eg>
2025-05-14 22:50:01 +0300

    fix: Update Dockerfile to use consistent casing for build stage

--- backend/loginPage/obj/Debug/net8.0/loginPage.AssemblyInfo.cs
+++ backend/loginPage/obj/Debug/net8.0/loginPage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("loginPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dd3ef2a259facac205106d497317a63c9936ab2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc0f70b502e3504b9498471586f7d53ee514e91")]
 [assembly: System.Reflection.AssemblyProductAttribute("loginPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("loginPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
